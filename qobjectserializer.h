c0097c5cc272e7af8f01454ee490515f0472daf0
Prashanth Udupa <prashanth.udupa@gmail.com>
2020-04-14 22:19:49 +0530

    Updated copyright notice in headers.

--- qobjectserializer.h
+++ qobjectserializer.h
@@ -1,7 +1,7 @@
 /****************************************************************************
 **
-** Copyright (C) Prashanth Udupa, Bengaluru
-** Email: prashanth.udupa@gmail.com
+** Copyright (C) TERIFLIX Entertainment Spaces Pvt. Ltd. Bengaluru
+** Author: Prashanth N Udupa (prashanth.udupa@teriflix.com)
 **
 ** This code is distributed under GPL v3. Complete text of the license
 ** can be found here: https://www.gnu.org/licenses/gpl-3.0.txt
