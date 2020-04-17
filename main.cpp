971bf7587602df8de423e7eaef5ed7a261bc00a3
Prashanth Udupa <prashanth@vcreatelogic.com>
2020-04-17 18:20:03 +0530

    Bumped version number to 0.2.8-Beta

--- main.cpp
+++ main.cpp
@@ -41,7 +41,7 @@
 
 int main(int argc, char **argv)
 {
-    const QVersionNumber applicationVersion(0, 2, 7);
+    const QVersionNumber applicationVersion(0, 2, 8);
     Application::setApplicationName("scrite");
     Application::setOrganizationName("TERIFLIX");
     Application::setOrganizationDomain("teriflix.com");
