e533862d25f5b3537e4d579c06b5d51233c6dada
Prashanth Udupa <prashanth@vcreatelogic.com>
2020-05-21 16:58:03 +0530

    Package batch file for Windows now performs code signing of the installer.

--- packaging/windows/package.bat
+++ packaging/windows/package.bat
@@ -1,3 +1,5 @@
 copy ..\..\..\Release\scrite.exe .
 copy vcredist_x64 vcredist_x64.exe
 windeployqt --qmldir ..\..\qml --no-compiler-runtime .
+%MakeNSISTool% installer.nsi
+%CodeSignTool% sign /f %TERIFLIX_CSC% /p %TERIFLIX_CSC_PWORD% Scrite-*-Beta-Setup.exe
