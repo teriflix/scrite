e3b045814ae05faf7564589fa5294e549b730246
Prashanth Udupa <prashanth.udupa@gmail.com>
2020-05-25 12:59:15 +0530

    Bumping version number to 0.3.6

--- packaging/mac/packaging.sh
+++ packaging/mac/packaging.sh
@@ -1,3 +1,3 @@
 cp -vaRf ../../../Release/scrite.app .
-~/Qt5.13.2/5.13.2/clang_64/bin/macdeployqt scrite.app -qmldir=../../qml -verbose=1 -appstore-compliant -dmg
-mv scrite.dmg scrite-0.3.5-beta.dmg
+~/Qt5.14.2/5.14.2/clang_64/bin/macdeployqt scrite.app -qmldir=../../qml -verbose=1 -appstore-compliant -dmg
+mv scrite.dmg scrite-0.3.6-beta.dmg
