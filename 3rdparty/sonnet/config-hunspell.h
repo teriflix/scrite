6af8f5fde901f4f33d34e450104dbb37f53f2db2
Prashanth Udupa <prashanth.udupa@gmail.com>
2020-06-21 17:11:14 +0530

    Implementing [0.4.1] Spellcheck #100 We generate Scrite builds using Ubuntu 16.04, on which we only get libhunspell-1.3. The README and config-hunspell.h files have been updated to reflect this.

--- 3rdparty/sonnet/config-hunspell.h
+++ 3rdparty/sonnet/config-hunspell.h
@@ -1,1 +1,5 @@
-#define USE_OLD_HUNSPELL_API 0
+// if you are using any version >= libhunspell-1.5
+// #define USE_OLD_HUNSPELL_API 0
+
+// If you are using any version < libhunspell-1.5
+#define USE_OLD_HUNSPELL_API 1
