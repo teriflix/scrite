7a30f32938c24af9f5f8441cd6be1849abfff25e
Prashanth Udupa <prashanth.udupa@gmail.com>
2020-08-16 18:15:51 +0530

    bumping version number to 0.4.16 - to ensure that source code & nightly builds are more recent than the published beta.

--- main.cpp
+++ main.cpp
@@ -91,7 +91,7 @@ void ScriteQtMessageHandler(QtMsgType type, const QMessageLogContext & context, 
 
 int main(int argc, char **argv)
 {
-    const QVersionNumber applicationVersion(0, 4, 15);
+    const QVersionNumber applicationVersion(0, 4, 16);
     Application::setApplicationName("Scrite");
     Application::setOrganizationName("TERIFLIX");
     Application::setOrganizationDomain("teriflix.com");
