35533bdc1a846c07e7968297ce462db17061cea1
Prashanth Udupa <prashanth.udupa@gmail.com>
2020-10-12 13:40:58 +0530

    Bumping version to 0.5.1

--- main.cpp
+++ main.cpp
@@ -94,7 +94,7 @@ void ScriteQtMessageHandler(QtMsgType type, const QMessageLogContext & context, 
 
 int main(int argc, char **argv)
 {
-    const QVersionNumber applicationVersion(0, 5, 0);
+    const QVersionNumber applicationVersion(0, 5, 1);
     Application::setApplicationName("Scrite");
     Application::setOrganizationName("TERIFLIX");
     Application::setOrganizationDomain("teriflix.com");
