22f27a103e7a8d11f429412c3c7db6c1c0dd5148
Prashanth Udupa <prashanth.udupa@gmail.com>
2020-12-19 13:27:04 +0530

    We bump version number to 0.5.4 - so that all nightly build users can also upgrade to this version.

--- main.cpp
+++ main.cpp
@@ -96,7 +96,7 @@ void ScriteQtMessageHandler(QtMsgType type, const QMessageLogContext & context, 
 
 int main(int argc, char **argv)
 {
-    const QVersionNumber applicationVersion(0, 5, 3);
+    const QVersionNumber applicationVersion(0, 5, 4);
     Application::setApplicationName("Scrite");
     Application::setOrganizationName("TERIFLIX");
     Application::setOrganizationDomain("teriflix.com");
