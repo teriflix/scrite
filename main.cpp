ba601cf4c1734e646d1b58eed2d49fb6836a6beb
Prashanth Udupa <prashanth.udupa@gmail.com>
2021-06-02 14:23:50 +0530

    Bumping to 0.6.8, calling this a public beta now.

--- main.cpp
+++ main.cpp
@@ -103,7 +103,7 @@ void ScriteQtMessageHandler(QtMsgType type, const QMessageLogContext & context, 
 
 int main(int argc, char **argv)
 {
-    const QVersionNumber applicationVersion(0, 6, 7);
+    const QVersionNumber applicationVersion(0, 6, 8);
     Application::setApplicationName("Scrite");
     Application::setOrganizationName("TERIFLIX");
     Application::setOrganizationDomain("teriflix.com");
