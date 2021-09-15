cb59ecb70086372004d1d991b7fc3c485c122a8c
Prashanth Udupa <prashanth.udupa@gmail.com>
2021-09-15 10:16:11 +0530

    Bumping version number to 0.7.12

--- main.cpp
+++ main.cpp
@@ -107,7 +107,7 @@ void ScriteQtMessageHandler(QtMsgType type, const QMessageLogContext & context, 
 
 int main(int argc, char **argv)
 {
-    const QVersionNumber applicationVersion(0, 7, 11);
+    const QVersionNumber applicationVersion(0, 7, 12);
     Application::setApplicationName(QStringLiteral("Scrite"));
     Application::setOrganizationName(QStringLiteral("TERIFLIX"));
     Application::setOrganizationDomain(QStringLiteral("teriflix.com"));
