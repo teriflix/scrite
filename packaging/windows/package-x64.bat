6dfed3bb9f264ac70c3b0fb9d43fe00385bebdc0
Prashanth Udupa <prashanth@vcreatelogic.com>
2021-11-29 11:29:17 +0530

    Updated package scripts on Windows to include OpenSSL-libs in the installer.

--- packaging/windows/package-x64.bat
+++ packaging/windows/package-x64.bat
@@ -1,5 +1,7 @@
 copy ..\..\..\Release\scrite.exe .
 copy C:\Qt\Qt5.13.2\vcredist\vcredist_msvc2017_x64.exe vcredist_x64.exe
+copy %SCRITE_OPENSSL_LIBS%\openssl-1.1\x64\bin\libcrypto-1_1-x64.dll .
+copy %SCRITE_OPENSSL_LIBS%\openssl-1.1\x64\bin\libssl-1_1-x64.dll .
 windeployqt --qmldir ..\..\qml --no-compiler-runtime --no-translations . --list relative > files.txt
 fillnsi --installs-key WINDEPLOYQT_INSTALLS --uninstalls-key WINDEPLOYQT_UNINSTALLS --list files.txt --input installer-x64.nsi.in --output installer-x64.nsi
 %MakeNSISTool% installer-x64.nsi
--- packaging/windows/package-x86.bat
+++ packaging/windows/package-x86.bat
@@ -1,5 +1,7 @@
 copy ..\..\..\Release\scrite.exe .
 copy C:\Qt\Qt5.13.2\vcredist\vcredist_msvc2017_x86.exe vcredist_x86.exe
+copy %SCRITE_OPENSSL_LIBS%\openssl-1.1\x86\bin\libcrypto-1_1.dll .
+copy %SCRITE_OPENSSL_LIBS%\openssl-1.1\x86\bin\libssl-1_1.dll .
 windeployqt --qmldir ..\..\qml --no-compiler-runtime --no-translations . --list relative > files.txt
 fillnsi --installs-key WINDEPLOYQT_INSTALLS --uninstalls-key WINDEPLOYQT_UNINSTALLS --list files.txt --input installer-x86.nsi.in --output installer-x86.nsi
 %MakeNSISTool% installer-x86.nsi
