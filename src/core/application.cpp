0a2b62b37a6f9b56138e47d6af8a41c010c27c60
Prashanth Udupa <prashanth.udupa@gmail.com>
2022-01-13 12:46:45 +0530

    Bumping version number to 0.8.5

--- src/core/application.cpp
+++ src/core/application.cpp
@@ -188,7 +188,7 @@ Application::Application(int &argc, char **argv, const QVersionNumber &version)
 
 QVersionNumber Application::prepare()
 {
-    const QVersionNumber applicationVersion(0, 8, 4);
+    const QVersionNumber applicationVersion(0, 8, 5);
 
     if (qApp != nullptr)
         return applicationVersion;
