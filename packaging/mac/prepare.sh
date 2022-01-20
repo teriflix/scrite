176f8b84ef59b2cf55d53c9d21edfd37e61e80d7
Prashanth Udupa <prashanth.udupa@gmail.com>
2022-01-20 11:46:07 +0530

    Updated prepare.sh to use Qt 5.15.8

--- packaging/mac/prepare.sh
+++ packaging/mac/prepare.sh
@@ -1,4 +1,4 @@
 cp -vaRf ../../../Release/Scrite.app .
 cp -vaf ../../Info.plist Scrite.app/Contents
-~/Qt/5.15.7/clang_64/bin/macdeployqt Scrite.app -qmldir=../../qml -verbose=1 -appstore-compliant -hardened-runtime -codesign="$TERIFLIX_IDENT"
+~/Qt/5.15.8/clang_64/bin/macdeployqt Scrite.app -qmldir=../../qml -verbose=1 -appstore-compliant -hardened-runtime -codesign="$TERIFLIX_IDENT"
 
