e3eb1e03019864f975c18e5a319947eb0a973399
Prashanth Udupa <prashanth.udupa@gmail.com>
2023-02-17 10:55:36 +0530

    Bumped version number to 0.9.3i

--- src/core/application.cpp
+++ src/core/application.cpp
@@ -236,7 +236,7 @@ static void copyFilesRecursively(const QDir &from, const QDir &to)
 
 QVersionNumber Application::prepare()
 {
-    const QVersionNumber applicationVersion = QVersionNumber::fromString(QStringLiteral("0.9.3.8"));
+    const QVersionNumber applicationVersion = QVersionNumber::fromString(QStringLiteral("0.9.3.9"));
     const QString applicationVersionString = [applicationVersion]() -> QString {
         const QVector<int> segments = applicationVersion.segments();
 
