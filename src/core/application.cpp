80669ad6b00426030e840c8d262a82b8e104e52c
Prashanth Udupa <prashanth.udupa@gmail.com>
2023-03-10 16:43:00 +0530

    Bumping version to 0.9.3n. Time for a new nightly build.

--- src/core/application.cpp
+++ src/core/application.cpp
@@ -237,7 +237,7 @@ static void copyFilesRecursively(const QDir &from, const QDir &to)
 QVersionNumber Application::prepare()
 {
     const QVersionNumber applicationVersion =
-            QVersionNumber::fromString(QStringLiteral("0.9.3.13"));
+            QVersionNumber::fromString(QStringLiteral("0.9.3.14"));
     const QString applicationVersionString = [applicationVersion]() -> QString {
         const QVector<int> segments = applicationVersion.segments();
 
