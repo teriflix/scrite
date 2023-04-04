3fe1ec24b54a1a161b6e985a68eebfa833e75690
Prashanth Udupa <prashanth.udupa@gmail.com>
2023-04-04 17:56:54 +0530

    Switching to Qt 5.15.13 LTS on macOS

--- packaging/mac/package.sh
+++ packaging/mac/package.sh
@@ -1,12 +1,12 @@
 cp -vaRf ../../../Release/Scrite.app .
 cp -vaf ../../Info.plist Scrite.app/Contents
-~/Qt/5.15.12/clang_64/bin/macdeployqt Scrite.app -qmldir=../../qml -verbose=1 -appstore-compliant -hardened-runtime -codesign="$SCRITE_IDENT"
+~/Qt/5.15.13/clang_64/bin/macdeployqt Scrite.app -qmldir=../../qml -verbose=1 -appstore-compliant -hardened-runtime -codesign="$SCRITE_IDENT"
 mkdir Scrite-0.9.3t-beta
 mv Scrite.app Scrite-0.9.3t-beta
 cp ../../images/dmgbackdrop.png dmgbackdrop.png
 cp ../../appicon.icns Scrite.icns
 sed "s/{{VERSION}}/Version 0.9.3t Beta/" dmgbackdrop.qml > dmgbackdropgen.qml
-~/Qt/5.15.12/clang_64/bin/qmlscene dmgbackdropgen.qml
+~/Qt/5.15.13/clang_64/bin/qmlscene dmgbackdropgen.qml
 rm -f dmgbackdropgen.qml
 
 # https://ss64.com/osx/sips.html
