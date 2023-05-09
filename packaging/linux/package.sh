1c62bea2c300e56c2d5c16e6cd6c02d0f562dfe0
Prashanth Udupa <prashanth.udupa@gmail.com>
2023-05-09 10:46:18 +0530

    Bumping version number to 0.9.3w

--- packaging/linux/package.sh
+++ packaging/linux/package.sh
@@ -1,8 +1,8 @@
 export PATH=/home/prashanthudupa/Qt/5.15.10/gcc_64/bin:/usr/lib/x86_64-linux-gnu:$PATH
 export LD_LIBRARY_PATH=export LD_LIBRARY_PATH=/home/prashanthudupa/Qt/5.15.10/gcc_64/lib:/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
-export VERSION=0.9.3v-beta
-mkdir Scrite-0.9.3v-beta.AppImage
-cd Scrite-0.9.3v-beta.AppImage
+export VERSION=0.9.3w-beta
+mkdir Scrite-0.9.3w-beta.AppImage
+cd Scrite-0.9.3w-beta.AppImage
 mkdir bin
 cp ../../../../Release/Scrite ./bin/
 mkdir -p share/applications
@@ -12,5 +12,5 @@ cp ../../../images/appicon.png share/icons/hicolor/512x512/apps/Scrite.png
 mkdir -p share/icons/hicolor/256x256/apps/
 convert ../../../images/appicon.png -resize 256x256 share/icons/hicolor/256x256/apps/Scrite.png
 cd ../
-linuxdeployqt Scrite-0.9.3v-beta.AppImage/share/applications/Scrite.desktop -appimage -qmldir=../../qml -verbose=2 -no-translations -no-copy-copyright-files
+linuxdeployqt Scrite-0.9.3w-beta.AppImage/share/applications/Scrite.desktop -appimage -qmldir=../../qml -verbose=2 -no-translations -no-copy-copyright-files
 cd ../
