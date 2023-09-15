cfce2e34fede81f4bedca4ee502030b75593689a
Prashanth Udupa <prashanth.udupa@gmail.com>
2023-09-15 11:27:18 +0530

    Fixed CodeSigning process on Windows
    On Windows, we need to signe scrite.exe before it goes into the
    installer, which should also be separately signed as well.

--- packaging/windows/package-x64.bat
+++ packaging/windows/package-x64.bat
@@ -1,8 +1,9 @@
 copy ..\..\..\Release\scrite.exe .
+%CodeSignTool% sign /tr http://timestamp.sectigo.com /td sha256 /fd sha256 /a scrite.exe
 copy C:\Qt\vcredist\vcredist_msvc2019_x64.exe vcredist_x64.exe
 copy %SCRITE_OPENSSL_LIBS%\openssl-1.1\x64\bin\libcrypto-1_1-x64.dll .
 copy %SCRITE_OPENSSL_LIBS%\openssl-1.1\x64\bin\libssl-1_1-x64.dll .
 windeployqt --qmldir ..\..\qml --no-compiler-runtime --no-translations . --list relative > files.txt
 fillnsi --installs-key WINDEPLOYQT_INSTALLS --uninstalls-key WINDEPLOYQT_UNINSTALLS --list files.txt --input installer-x64.nsi.in --output installer-x64.nsi
 %MakeNSISTool% installer-x64.nsi
-%CodeSignTool% sign sign /tr http://timestamp.sectigo.com /td sha256 /fd sha256 /a Scrite-*-Beta-64bit-Setup.exe
+%CodeSignTool% sign /tr http://timestamp.sectigo.com /td sha256 /fd sha256 /a Scrite-*-Beta-64bit-Setup.exe
--- packaging/windows/package-x86.bat
+++ packaging/windows/package-x86.bat
@@ -1,8 +1,9 @@
 copy ..\..\..\Release\scrite.exe .
+%CodeSignTool% sign /tr http://timestamp.sectigo.com /td sha256 /fd sha256 /a scrite.exe
 copy C:\Qt\vcredist\vcredist_msvc2019_x86.exe vcredist_x86.exe
 copy %SCRITE_OPENSSL_LIBS%\openssl-1.1\x86\bin\libcrypto-1_1.dll .
 copy %SCRITE_OPENSSL_LIBS%\openssl-1.1\x86\bin\libssl-1_1.dll .
 windeployqt --qmldir ..\..\qml --no-compiler-runtime --no-translations . --list relative > files.txt
 fillnsi --installs-key WINDEPLOYQT_INSTALLS --uninstalls-key WINDEPLOYQT_UNINSTALLS --list files.txt --input installer-x86.nsi.in --output installer-x86.nsi
 %MakeNSISTool% installer-x86.nsi
-%CodeSignTool% sign sign /tr http://timestamp.sectigo.com /td sha256 /fd sha256 /a Scrite-*-Beta-32bit-Setup.exe
+%CodeSignTool% sign /tr http://timestamp.sectigo.com /td sha256 /fd sha256 /a Scrite-*-Beta-32bit-Setup.exe
