6a1009d9a6de57a91479ec5a4b134d1f10b6c9a6
Prashanth N Udupa <prashanth@vcreatelogic.com>
2024-04-29 22:54:35 +0530

    Bumping version number to 0.9.4t

--- src/core/application.cpp
+++ src/core/application.cpp
@@ -232,7 +232,7 @@ static void copyFilesRecursively(const QDir &from, const QDir &to)
 QVersionNumber Application::prepare()
 {
     const QVersionNumber applicationVersion =
-            QVersionNumber::fromString(QStringLiteral("0.9.4.19"));
+            QVersionNumber::fromString(QStringLiteral("0.9.4.20"));
     const QString applicationVersionString = [applicationVersion]() -> QString {
         const QVector<int> segments = applicationVersion.segments();
 
