9c588c3f9c8087de0cead0315187913e70e8423f
Prashanth N Udupa <prashanth@vcreatelogic.com>
2024-06-06 21:48:31 +0530

    Bumping to 0.9.5g for a new nightly build.

--- src/core/application.cpp
+++ src/core/application.cpp
@@ -231,7 +231,7 @@ static void copyFilesRecursively(const QDir &from, const QDir &to)
 
 QVersionNumber Application::prepare()
 {
-    const QVersionNumber applicationVersion = QVersionNumber::fromString(QStringLiteral("0.9.5.6"));
+    const QVersionNumber applicationVersion = QVersionNumber::fromString(QStringLiteral("0.9.5.7"));
     const QString applicationVersionString = [applicationVersion]() -> QString {
         const QVector<int> segments = applicationVersion.segments();
 
