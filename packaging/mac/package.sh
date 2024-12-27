314c98b8163da478e8eaea5794d6bd4e05a0add9
Prashanth N Udupa <prashanth@vcreatelogic.com>
2024-12-27 16:26:16 +0530

    Bumping version number to 0.9.9e

--- packaging/mac/package.sh
+++ packaging/mac/package.sh
@@ -4,11 +4,11 @@ cp -vaRf ../../../Release/Scrite.app .
 codesign -s "$SCRITE_IDENT" ./Scrite.app/Contents/MacOS/crashpad_handler
 cp -vaf ../../Info.plist Scrite.app/Contents
 ~/Qt/5.15.14/clang_64/bin/macdeployqt Scrite.app -qmldir=../../qml -verbose=1 -appstore-compliant -hardened-runtime -codesign="$SCRITE_IDENT"
-mkdir Scrite-0.9.9d-beta
-mv Scrite.app Scrite-0.9.9d-beta
+mkdir Scrite-0.9.9e-beta
+mv Scrite.app Scrite-0.9.9e-beta
 cp ../../images/dmgbackdrop.png dmgbackdrop.png
 cp ../../appicon.icns Scrite.icns
-sed "s/{{VERSION}}/Version 0.9.9d Beta/" dmgbackdrop.qml > dmgbackdropgen.qml
+sed "s/{{VERSION}}/Version 0.9.9e Beta/" dmgbackdrop.qml > dmgbackdropgen.qml
 ~/Qt/5.15.14/clang_64/bin/qmlscene dmgbackdropgen.qml
 rm -f dmgbackdropgen.qml
 
@@ -18,7 +18,7 @@ sips -s dpiWidth 144 -s dpiHeight 144 background.png
 # https://github.com/create-dmg/create-dmg
 ~/Utils/create-dmg/create-dmg \
   --volicon "Scrite.icns" \
-  --volname "Scrite-0.9.9d-beta" \
+  --volname "Scrite-0.9.9e-beta" \
   --background "background.png" \
   --window-pos 272 136 \
   --window-size 896 660 \
@@ -27,12 +27,12 @@ sips -s dpiWidth 144 -s dpiHeight 144 background.png
   --hide-extension "Scrite.app" \
   --app-drop-link 620 300 \
   --hdiutil-verbose \
-  "Scrite-0.9.9d-beta.dmg" \
-  "Scrite-0.9.9d-beta/"
+  "Scrite-0.9.9e-beta.dmg" \
+  "Scrite-0.9.9e-beta/"
 rm -f background.png
 rm -f dmgbackdrop.png
 rm -f Scrite.icns
-mv Scrite-0.9.9d-beta/Scrite.app .
-rm -fr Scrite-0.9.9d-beta
+mv Scrite-0.9.9e-beta/Scrite.app .
+rm -fr Scrite-0.9.9e-beta
 
-mv ../../../Release/Scrite.app.sym Scrite-0.9.9d-beta.app.sym
+mv ../../../Release/Scrite.app.sym Scrite-0.9.9e-beta.app.sym
--- scrite.pro
+++ scrite.pro
@@ -5,7 +5,7 @@ TARGET = Scrite
 CONFIG += c++17
 DEFINES += PHTRANSLATE_STATICLIB
 
-VERSION = 0.9.9.4
+VERSION = 0.9.9.5
 DEFINES += SCRITE_VERSION=\\\"$$VERSION\\\"
 
 #DEFINES += SCRITE_ENABLE_AUTOMATION
@@ -337,7 +337,7 @@ QTQUICK_COMPILER_SKIPPED_RESOURCES += scrite_misc.qrc
 macx {
     ICON = appicon.icns
     QMAKE_INFO_PLIST = Info.plist
-    VERSION_INFO = "0.9.9d-beta-macos"
+    VERSION_INFO = "0.9.9e-beta-macos"
 
     HEADERS += src/core/systemtextinputmanager_macos.h
     OBJECTIVE_SOURCES += src/core/systemtextinputmanager_macos.mm
@@ -347,9 +347,9 @@ macx {
 
 win32 {
     contains(QT_ARCH, i386) {
-        VERSION_INFO = "0.9.9d-beta-windows-x86"
+        VERSION_INFO = "0.9.9e-beta-windows-x86"
     } else {
-        VERSION_INFO = "0.9.9d-beta-windows-x64"
+        VERSION_INFO = "0.9.9e-beta-windows-x64"
     }
 
     RC_ICONS = appicon.ico
@@ -360,7 +360,7 @@ win32 {
 
 linux {
     CONFIG+=use_gold_linker
-    VERSION_INFO = "0.9.9d-beta-linux"
+    VERSION_INFO = "0.9.9e-beta-linux"
 }
 
 include($$PWD/3rdparty/sonnet/sonnet.pri)
