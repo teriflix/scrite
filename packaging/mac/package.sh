945a036100c0c58b2ce33906160dde88de6e6396
Prashanth N Udupa <prashanth@vcreatelogic.com>
2025-01-13 11:39:47 +0530

    Bumping version number to 1.0

--- packaging/mac/package.sh
+++ packaging/mac/package.sh
@@ -4,11 +4,11 @@ cp -vaRf ../../../Release/Scrite.app .
 codesign -s "$SCRITE_IDENT" ./Scrite.app/Contents/MacOS/crashpad_handler
 cp -vaf ../../Info.plist Scrite.app/Contents
 ~/Qt/5.15.18/clang_64/bin/macdeployqt Scrite.app -qmldir=../../qml -verbose=1 -appstore-compliant -hardened-runtime -codesign="$SCRITE_IDENT"
-mkdir Scrite-0.9.9i-beta
-mv Scrite.app Scrite-0.9.9i-beta
+mkdir Scrite-1.0
+mv Scrite.app Scrite-1.0
 cp ../../images/dmgbackdrop.png dmgbackdrop.png
 cp ../../appicon.icns Scrite.icns
-sed "s/{{VERSION}}/Version 0.9.9i Beta/" dmgbackdrop.qml > dmgbackdropgen.qml
+sed "s/{{VERSION}}/Version 1.0/" dmgbackdrop.qml > dmgbackdropgen.qml
 ~/Qt/5.15.18/clang_64/bin/qmlscene dmgbackdropgen.qml
 rm -f dmgbackdropgen.qml
 
@@ -18,7 +18,7 @@ sips -s dpiWidth 144 -s dpiHeight 144 background.png
 # https://github.com/create-dmg/create-dmg
 ~/Utils/create-dmg/create-dmg \
   --volicon "Scrite.icns" \
-  --volname "Scrite-0.9.9i-beta" \
+  --volname "Scrite-1.0" \
   --background "background.png" \
   --window-pos 272 136 \
   --window-size 896 660 \
@@ -27,12 +27,12 @@ sips -s dpiWidth 144 -s dpiHeight 144 background.png
   --hide-extension "Scrite.app" \
   --app-drop-link 620 300 \
   --hdiutil-verbose \
-  "Scrite-0.9.9i-beta.dmg" \
-  "Scrite-0.9.9i-beta/"
+  "Scrite-1.0.dmg" \
+  "Scrite-1.0/"
 rm -f background.png
 rm -f dmgbackdrop.png
 rm -f Scrite.icns
-mv Scrite-0.9.9i-beta/Scrite.app .
-rm -fr Scrite-0.9.9i-beta
+mv Scrite-1.0/Scrite.app .
+rm -fr Scrite-1.0
 
-mv ../../../Release/Scrite.app.sym Scrite-0.9.9i-beta.app.sym
+mv ../../../Release/Scrite.app.sym Scrite-1.0.app.sym
