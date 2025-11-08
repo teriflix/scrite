74d340e6934484abe6c51965b1e190d17bf7f1e2
Prashanth Udupa <prashanth.udupa@gmail.com>
2025-11-08 19:10:29 +0530

    [build] Updating build scripts for macOS and Linux to use Qt 5.15.19

--- packaging/linux/package.sh
+++ packaging/linux/package.sh
@@ -1,5 +1,5 @@
-export PATH=/home/prashanthudupa/Qt/5.15.18/gcc_64/bin:/usr/lib/x86_64-linux-gnu:$PATH
-export LD_LIBRARY_PATH=/home/prashanthudupa/Qt/5.15.18/gcc_64/lib:/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
+export PATH=/home/prashanthudupa/Qt/5.15.19/gcc_64/bin:/usr/lib/x86_64-linux-gnu:$PATH
+export LD_LIBRARY_PATH=/home/prashanthudupa/Qt/5.15.19/gcc_64/lib:/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
 export VERSION=1.9.0
 mkdir Scrite-1.9.0.AppImage
 cd Scrite-1.9.0.AppImage
--- packaging/mac/package.sh
+++ packaging/mac/package.sh
@@ -3,13 +3,13 @@ $SCRITE_CRASHPAD_ROOT/bin/dump_syms ../../../Release/Scrite.app.dSYM/Contents/Re
 cp -vaRf ../../../Release/Scrite.app .
 codesign -s "$SCRITE_IDENT" ./Scrite.app/Contents/MacOS/crashpad_handler
 cp -vaf ../../Info.plist Scrite.app/Contents
-~/Qt/5.15.18/clang_64/bin/macdeployqt Scrite.app -qmldir=../../qml -verbose=1 -appstore-compliant -hardened-runtime -codesign="$SCRITE_IDENT"
+~/Qt/5.15.19/clang_64/bin/macdeployqt Scrite.app -qmldir=../../qml -verbose=1 -appstore-compliant -hardened-runtime -codesign="$SCRITE_IDENT"
 mkdir Scrite-1.9.0
 mv Scrite.app Scrite-1.9.0
 cp ../../images/dmgbackdrop.png dmgbackdrop.png
 cp ../../appicon.icns Scrite.icns
 sed "s/{{VERSION}}/Version 1.9.0/" dmgbackdrop.qml > dmgbackdropgen.qml
-~/Qt/5.15.18/clang_64/bin/qmlscene dmgbackdropgen.qml
+~/Qt/5.15.19/clang_64/bin/qmlscene dmgbackdropgen.qml
 rm -f dmgbackdropgen.qml
 
 # https://ss64.com/osx/sips.html
