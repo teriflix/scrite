7f2149f00c45c0ab558047586f6b5b8c2975ebce
Prashanth N Udupa <prashanth@vcreatelogic.com>
2025-11-24 21:53:02 +0530

    [packaging]: Updated code-sign command options in package scripts for Windows
    Previously SignTool /a was acceptable, but now it appears we should
    explicitly state the business name as well.
    
    So, now we use the /n flag to specify business name while packaging.

--- packaging/windows/package-x64.bat
+++ packaging/windows/package-x64.bat
@@ -1,5 +1,5 @@
 copy ..\..\..\Release\scrite.exe .
-%CodeSignTool% sign /tr http://timestamp.sectigo.com /td sha256 /fd sha256 /a scrite.exe
+%CodeSignTool% sign /tr http://timestamp.sectigo.com /td sha256 /fd sha256 /n "%SCRITE_BUSINESS_NAME%" scrite.exe
 copy C:\Qt\vcredist\vcredist_msvc2022_x64.exe vcredist_x64.exe
 copy %SCRITE_OPENSSL_LIBS%\openssl-1.1\x64\bin\libcrypto-1_1-x64.dll .
 copy %SCRITE_OPENSSL_LIBS%\openssl-1.1\x64\bin\libssl-1_1-x64.dll .
@@ -7,7 +7,7 @@ windeployqt --qmldir ..\..\qml --no-compiler-runtime --no-translations . --list 
 copy %SCRITE_CRASHPAD_ROOT%\bin\crashpad_handler.exe .
 fillnsi --installs-key WINDEPLOYQT_INSTALLS --uninstalls-key WINDEPLOYQT_UNINSTALLS --list files.txt --input installer-x64.nsi.in --output installer-x64.nsi
 %MakeNSISTool% installer-x64.nsi
-%CodeSignTool% sign /tr http://timestamp.sectigo.com /td sha256 /fd sha256 /a Scrite-1.9.7-64bit-Setup.exe
+%CodeSignTool% sign /tr http://timestamp.sectigo.com /td sha256 /fd sha256 /n "%SCRITE_BUSINESS_NAME%" Scrite-1.9.7-64bit-Setup.exe
 mkdir ..\..\..\Release\Deploy
 copy Scrite-1.9.7-64bit-Setup.exe ..\..\..\Release\Deploy
 copy ..\..\..\Release\scrite.pdb ..\..\..\Release\Deploy\Scrite-1.9.7-64bit.pdb
--- packaging/windows/package-x86.bat
+++ packaging/windows/package-x86.bat
@@ -1,5 +1,5 @@
 copy ..\..\..\Release\scrite.exe .
-%CodeSignTool% sign /tr http://timestamp.sectigo.com /td sha256 /fd sha256 /a scrite.exe
+%CodeSignTool% sign /tr http://timestamp.sectigo.com /td sha256 /fd sha256 /n "%SCRITE_BUSINESS_NAME%" scrite.exe
 copy C:\Qt\vcredist\vcredist_msvc2019_x86.exe vcredist_x86.exe
 copy %SCRITE_OPENSSL_LIBS%\openssl-1.1\x86\bin\libcrypto-1_1.dll .
 copy %SCRITE_OPENSSL_LIBS%\openssl-1.1\x86\bin\libssl-1_1.dll .
@@ -7,7 +7,7 @@ windeployqt --qmldir ..\..\qml --no-compiler-runtime --no-translations . --list 
 copy %SCRITE_CRASHPAD_ROOT%\bin\crashpad_handler.exe .
 fillnsi --installs-key WINDEPLOYQT_INSTALLS --uninstalls-key WINDEPLOYQT_UNINSTALLS --list files.txt --input installer-x86.nsi.in --output installer-x86.nsi
 %MakeNSISTool% installer-x86.nsi
-%CodeSignTool% sign /tr http://timestamp.sectigo.com /td sha256 /fd sha256 /a Scrite-1.9.7-32bit-Setup.exe
+%CodeSignTool% sign /tr http://timestamp.sectigo.com /td sha256 /fd sha256 /n "%SCRITE_BUSINESS_NAME%" Scrite-1.9.7-64bit-Setup.exe
 mkdir ..\..\..\Release\Deploy
 copy Scrite-1.9.7-32bit-Setup.exe ..\..\..\Release\Deploy
 copy ..\..\..\Release\scrite.pdb ..\..\..\Release\Deploy\Scrite-1.9.7-32bit.pdb
