c188a363edc29d3543e81b692175406327891e1e
Prashanth N Udupa <prashanth@vcreatelogic.com>
2025-12-06 15:57:45 +0530

    [build] Bumping version to 1.9.11 for a new beta

--- packaging/windows/package-x86.bat
+++ packaging/windows/package-x86.bat
@@ -7,7 +7,7 @@ windeployqt --qmldir ..\..\qml --no-compiler-runtime --no-translations . --list 
 copy %SCRITE_CRASHPAD_ROOT%\bin\crashpad_handler.exe .
 fillnsi --installs-key WINDEPLOYQT_INSTALLS --uninstalls-key WINDEPLOYQT_UNINSTALLS --list files.txt --input installer-x86.nsi.in --output installer-x86.nsi
 %MakeNSISTool% installer-x86.nsi
-%CodeSignTool% sign /tr http://timestamp.sectigo.com /td sha256 /fd sha256 /n "%SCRITE_BUSINESS_NAME%" Scrite-1.9.10-32bit-Setup.exe
+%CodeSignTool% sign /tr http://timestamp.sectigo.com /td sha256 /fd sha256 /n "%SCRITE_BUSINESS_NAME%" Scrite-1.9.11-32bit-Setup.exe
 mkdir ..\..\..\Release\Deploy
-copy Scrite-1.9.10-32bit-Setup.exe ..\..\..\Release\Deploy
-copy ..\..\..\Release\scrite.pdb ..\..\..\Release\Deploy\Scrite-1.9.10-32bit.pdb
+copy Scrite-1.9.11-32bit-Setup.exe ..\..\..\Release\Deploy
+copy ..\..\..\Release\scrite.pdb ..\..\..\Release\Deploy\Scrite-1.9.11-32bit.pdb
