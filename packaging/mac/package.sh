778c2e624a5b4030094249c7e5d5c4947c0d8c3b
Prashanth Udupa <prashanth.udupa@gmail.com>
2026-01-25 21:03:46 +0530

    [build] Bumping version number to 2.0.14

--- packaging/mac/package.sh
+++ packaging/mac/package.sh
@@ -4,25 +4,25 @@ cp -vaRf ../../../Release/Scrite.app .
 codesign -s "$SCRITE_IDENT" ./Scrite.app/Contents/MacOS/crashpad_handler
 cp -vaf ../../Info.plist Scrite.app/Contents
 ~/Qt/5.15.19/macos/bin/macdeployqt Scrite.app -qmldir=../../qml -verbose=1 -appstore-compliant -hardened-runtime -codesign="$SCRITE_IDENT"
-mkdir Scrite-2.0.13
-mv Scrite.app Scrite-2.0.13
+mkdir Scrite-2.0.14
+mv Scrite.app Scrite-2.0.14
 cp ../../images/dmgbackdrop.png dmgbackdrop.png
 cp ../../appicon.icns Scrite.icns
-sed "s/{{VERSION}}/Version 2.0.13/" dmgbackdrop.qml > dmgbackdropgen.qml
+sed "s/{{VERSION}}/Version 2.0.14/" dmgbackdrop.qml > dmgbackdropgen.qml
 ~/Qt/5.15.19/macos/bin/qmlscene dmgbackdropgen.qml
 rm -f dmgbackdropgen.qml
 
 # https://ss64.com/osx/sips.html
 sips -s dpiWidth 144 -s dpiHeight 144 background.png
 
-sed "s/{{VERSION}}/2.0.13/" dmg_settings_tmpl.py > dmg_settings.py
+sed "s/{{VERSION}}/2.0.14/" dmg_settings_tmpl.py > dmg_settings.py
 
-dmgbuild -s dmg_settings.py "Scrite-2.0.13" Scrite-2.0.13.dmg
+dmgbuild -s dmg_settings.py "Scrite-2.0.14" Scrite-2.0.14.dmg
 
 rm -f background.png
 rm -f dmgbackdrop.png
 rm -f Scrite.icns
-mv Scrite-2.0.13/Scrite.app .
-rm -fr Scrite-2.0.13
+mv Scrite-2.0.14/Scrite.app .
+rm -fr Scrite-2.0.14
 
-mv ../../../Release/Scrite.app.sym Scrite-2.0.13.app.sym
+mv ../../../Release/Scrite.app.sym Scrite-2.0.14.app.sym
